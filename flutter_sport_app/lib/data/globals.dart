9f81a14a3004dd73ad2e69edf42755f0ba46d04f
marionwilms <marion.wilms@live.fr>
2020-01-30 11:37:09 +0100

    get elemeents from the json and fix problems

--- flutter_sport_app/lib/data/globals.dart
+++ flutter_sport_app/lib/data/globals.dart
@@ -3,16 +3,6 @@ library my_prj.globals;
 import 'Serie.dart';
 import 'Exercise.dart';
 
-var title;
-
-//var exo1 = Exercise('etirement1', '30s');
-//var exo2 = Exercise('etirement2', '35s');
-//var exo3 = Exercise('squat', 'x30');
-//var exo4 = Exercise('abdos', 'x40');
-//var serie1=Serie('serie1', '20min', [exo1, exo2]);
-//var serie2=Serie('serie2', '25min', [exo3, exo4]);
-//var serie3=Serie('serie3', '23min', [exo1, exo4]);
-//List<Serie> serieL = [serie1, serie2, serie3];
 var serieListJson = '''
 [
   { "title": "serie1",
--- flutter_sport_app/lib/data/serie_parser.dart
+++ flutter_sport_app/lib/data/serie_parser.dart
@@ -1,5 +1,3 @@
-import 'dart:async' show Future;
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_sport_app/data/Exercise.dart';
 import 'dart:convert';
 import 'Serie.dart';
@@ -8,13 +6,16 @@ import 'globals.dart' as global;
 
 void loadSerie() {
   var serieList = jsonDecode(global.serieListJson);
-  List<Exercise> exoL = new List();
+  var serieI;
+  var exoJ;
   Serie serieInt;
   Exercise exoInt;
-  global.title = serieList[0]['title'];
   for (int i=0; i < serieList.length; i++)  {
-    for (int j=0; j < serieList[i]['exerciseList'].length; j++) {
-      exoInt=Exercise(serieList[i]['exerciseList'][j]['title'], serieList[i]['exerciseList'][j]['lenght']);
+    serieI = serieList[i];
+    List<Exercise> exoL = new List();
+    for (int j=0; j < serieI['exerciseList'].length; j++) {
+      exoJ = serieI['exerciseList'][j];
+      exoInt=Exercise(exoJ['title'], exoJ['lenght']);
       exoL.add(exoInt);
     }
     serieInt=Serie(serieList[i]['title'], serieList[i]['lenght'], exoL);
